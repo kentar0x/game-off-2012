63070d795059d2e84a0b5af8cce8c737be7e482a
Samuel G <gelisam@gmail.com>
2012-10-27 19:00:14 -0400

    Basic css-sprite test
    Use the arrow keys to browse the different sprites.

---
+++ js/keycodes.js
@@ -0,0 +1,7 @@
+// http://unixpapa.com/js/key.html
+var keycodes = {
+  left: 37,
+  up: 38,
+  right: 39,
+  down: 40,
+};
--- js/octocarina.js
+++ js/octocarina.js
@@ -3,10 +3,43 @@ $(function() {
       return $('#content');
     }
     
+    var current_tile_type = 0;
+    var tile_types = ["empty-tile", "wall-tile", "block-sprite", "goal-sprite", "player-sprite"];
+    var current_tile = $('<div class="sprite"/>');
+    
+    function change_tile_type(new_type) {
+      new_type = (new_type + tile_types.length) % tile_types.length;
+      
+      current_tile.removeClass(tile_types[current_tile_type]);
+      current_tile_type = new_type;
+      current_tile.addClass(tile_types[current_tile_type]);
+      
+      console.log(tile_types[current_tile_type]);
+    }
+    
+    function handleKey(key) {
+      switch (key) {
+      case keycodes.left: /* falls through */
+      case keycodes.up:
+        change_tile_type(current_tile_type - 1);
+        break;
+      
+      case keycodes.right: /* falls through */
+      case keycodes.down:
+        change_tile_type(current_tile_type + 1);
+        break;
+      }
+    }
+    
     $('#begin').click(function() {
       var container = toplevel_container();
       container.addClass('well');
       container.empty();
-      container.text('Nothing to see here yet.');
+      container.append(current_tile);
+      change_tile_type(0);
+    });
+    
+    $(document).keydown(function(e) {
+      handleKey(e['keyCode']);
     });
 });
