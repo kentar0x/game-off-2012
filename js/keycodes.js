a8c8cbc2a2a423ba98e93c73ed2fdf5528460a39
Samuel G <gelisam@gmail.com>
2012-10-27 19:33:43 -0400

    Keycodes for letters.

--- js/keycodes.js
+++ js/keycodes.js
@@ -3,5 +3,12 @@ var keycodes = {
   left: 37,
   up: 38,
   right: 39,
-  down: 40,
+  down: 40
 };
+
+(function() {
+  var s = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  for(var i=0; i<26; ++i) {
+    keycodes[s[i]] = s.charCodeAt(i);
+  }
+})();
