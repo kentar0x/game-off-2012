b65379bafd33d620db4b397f015df9b5b309ea85
Samuel G <gelisam@gmail.com>
2012-10-27 23:20:16 -0400

    Remove sprite-browsing code
    Arrow keys shouldn't alter the level once it's loaded.

--- js/octocarina.js
+++ js/octocarina.js
@@ -3,29 +3,19 @@ $(function() {
     var sprite_container = toplevel_container;
     
     
-    var current_tile_type = 0;
     var tile_symbols =         "."+         "#"+            "b"+           "*"+             "C";
     var tile_types = ["empty-tile", "wall-tile", "block-sprite", "goal-sprite", "player-sprite"];
-    var current_tile = $('<div class="sprite"/>');
     
     function parse_tile_type(tile) {
       return tile_symbols.indexOf(tile);
     }
     
-    function change_tile_type(new_type) {
-      new_type = (new_type + tile_types.length) % tile_types.length;
-      
-      current_tile.removeClass(tile_types[current_tile_type]);
-      current_tile_type = new_type;
-      current_tile.addClass(tile_types[current_tile_type]);
-    }
-    
     function add_tile(tile_type) {
-      current_tile = $('<div class="sprite"/>');
-      change_tile_type(tile_type);
-      sprite_container.append(current_tile);
+      var cls = tile_types[tile_type];
+      var tile = $('<div class="sprite"/>').addClass(cls);
       
-      return current_tile;
+      sprite_container.append(tile);
+      return tile;
     }
     
     
@@ -51,21 +41,6 @@ $(function() {
     
     var keyHandler;
     function handleKey(key) {
-      switch (key) {
-      case keycodes.left: /* falls through */
-      case keycodes.up:
-        change_tile_type(current_tile_type - 1);
-        break;
-      
-      case keycodes.right: /* falls through */
-      case keycodes.down:
-        change_tile_type(current_tile_type + 1);
-        break;
-      
-      case keycodes.Z:
-        add_tile(current_tile_type);
-        break;
-      }
     }
     
     
