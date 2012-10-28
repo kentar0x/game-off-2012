72b5e34c2ab483f9cad874ed6c6c0a4712943ffb
Samuel G <gelisam@gmail.com>
2012-10-28 15:28:08 -0400

    Adding a layer of floor tiles

--- js/level-view.js
+++ js/level-view.js
@@ -3,22 +3,38 @@ var Level_View = {
     container.empty();
     
     var view = {
+      add_layer: function() {
+        this.current_layer = $('<div class="layer"/>');
+        
+        container.append(this.current_layer);
+        return this.current_layer;
+      },
       add_tile: function(tile_type) {
         var cls = Level_Data.class_for_tile_type(tile_type);
         var tile = $('<div class="sprite"/>').addClass(cls);
         
-        container.append(tile);
+        this.current_layer.append(tile);
         return tile;
       }
     };
     
     var tile_types = level_model.tiles;
+    var n = tile_types.length;
     
     var floor = Level_Data.type_for_tile_symbol('.');
     var empty = Level_Data.type_for_tile_symbol(' ');
     
+    // add the floor
+    view.add_layer();
+    for(var i=0; i<n; ++i) {
+      var tile_type = tile_types[i];
+      
+      view.add_tile(floor);
+    }
+    
     // convert the tile types into tile sprites
-    for(var i=0; i<tile_types.length; ++i) {
+    view.add_layer();
+    for(var i=0; i<n; ++i) {
       var tile_type = tile_types[i];
       
       if (tile_type == floor) {
