b2e783a829793e1861230ddc83f5e955aef152ae
Samuel G <gelisam@gmail.com>
2012-10-30 08:14:51 -0400

    Fixed bug triggered by forking too fast
    If you pressed "F" immediately after grabbing the star, you would end up
    with a miniature copy of the first level on top of the second level. We
    now prevent all keys, not just all movement, during the level-loading
    sequence.

--- js/octocarina.js
+++ js/octocarina.js
@@ -32,24 +32,22 @@ $(function() {
     
     
     function move_player(dx, dy) {
-      if (is_movement_allowed) {
-        var room = multiroom.current_room();
-        
-        var old_index = room.player_index();
-        var new_index = old_index.plus(dx, dy);
-        var new_index2 = old_index.plus(2*dx, 2*dy);
-        
-        var target = room.tile_at(new_index);
-        var target2 = room.tile_at(new_index2);
-        
-        if (target === Tile.goal) {
-          next_level();
-        } else if (target === Tile.block && !target2.solid) {
-          room.move_tile(new_index, new_index2);
-          room.move_player(new_index);
-        } else if (!target.solid) {
-          room.move_player(new_index);
-        }
+      var room = multiroom.current_room();
+      
+      var old_index = room.player_index();
+      var new_index = old_index.plus(dx, dy);
+      var new_index2 = old_index.plus(2*dx, 2*dy);
+      
+      var target = room.tile_at(new_index);
+      var target2 = room.tile_at(new_index2);
+      
+      if (target === Tile.goal) {
+        next_level();
+      } else if (target === Tile.block && !target2.solid) {
+        room.move_tile(new_index, new_index2);
+        room.move_player(new_index);
+      } else if (!target.solid) {
+        room.move_player(new_index);
       }
     }
     
@@ -65,16 +63,18 @@ $(function() {
     
     var keyHandler;
     function handleKey(key) {
-      switch(key) {
-      case Keycode.left:  return move_player(-1, 0);
-      case Keycode.right: return move_player( 1, 0);
-      case Keycode.up:    return move_player( 0,-1);
-      case Keycode.down:  return move_player( 0, 1);
-      
-      case Keycode.esc: /* falls through */
-      case Keycode.R: return try_again();
-      
-      case Keycode.F: return fork_room();
+      if (is_movement_allowed) {
+        switch(key) {
+        case Keycode.left:  return move_player(-1, 0);
+        case Keycode.right: return move_player( 1, 0);
+        case Keycode.up:    return move_player( 0,-1);
+        case Keycode.down:  return move_player( 0, 1);
+        
+        case Keycode.esc: /* falls through */
+        case Keycode.R: return try_again();
+        
+        case Keycode.F: return fork_room();
+        }
       }
     }
     
