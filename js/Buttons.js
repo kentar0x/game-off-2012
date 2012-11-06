c357ee8f3690da25e142cfea81b83a8965763666
Samuel G <gelisam@gmail.com>
2012-11-06 04:39:42 -0500

    Remove debugging statements.

--- js/Buttons.js
+++ js/Buttons.js
@@ -17,7 +17,6 @@ var Buttons = {
       
       if (tile.button) {
         ++button_count;
-        console.log(button_count);
         return Button.create(tile.color);
       } else {
         return null;
@@ -35,21 +34,16 @@ var Buttons = {
           var old_button = buttons.at(move.old_pos);
           var new_button = buttons.at(move.new_pos);
           
-          console.log(new_button);
-          
           if (old_button && old_button.remove_weight(move.moveable)) {
-            console.log('remove');
             --active_buttons;
           }
           if (new_button && new_button.add_weight(move.moveable)) {
-            console.log('add');
             ++active_buttons;
           }
         });
         
         // have things changed?
         var new_active = (active_buttons == button_count);
-        console.log(active_buttons);
         if (new_active != old_active) {
           if (new_active) {
             // open the door
