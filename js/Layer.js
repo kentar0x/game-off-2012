a4ab6d5e8eb125c8815e84866b3daf272b08368f
Samuel G <gelisam@gmail.com>
2012-11-08 22:01:49 -0500

    Fix indentation
    I don't have a very strong opinion regarding whether tabs or spaces are
    best, but mixed tabs+spaces in the same file, now that I cannot stand.

--- js/Layer.js
+++ js/Layer.js
@@ -6,22 +6,22 @@ var Layer = {
   create: function(container, tiles, room) {
     var element = $('<div class="layer"/>');
     container.append(element);
-	
-	var spriteIndex = 0;
-	var lines = new Array();
-	for (var i = 1; i <= room.h; i++)
-	{
-		var line = $("<div class='layer-line'></div>");
-		line.css('width', (room.w * 101) + 'px');
-		element.append(line);
-		lines.push(line);
-	}
-	
+    
+    var spriteIndex = 0;
+    var lines = new Array();
+    for (var i = 1; i <= room.h; i++)
+    {
+      var line = $("<div class='layer-line'></div>");
+      line.css('width', (room.w * 101) + 'px');
+      element.append(line);
+      lines.push(line);
+    }
+    
     var sprites = tiles.map(function(pos, tile) {
       return Sprite.create(lines[pos.y], tile);
     });
-	
-	var player = Sprite.create_player(container);
+    
+    var player = Sprite.create_player(container);
     
     return {
       sprite_at: function(pos) {
--- js/Sprite.js
+++ js/Sprite.js
@@ -3,28 +3,25 @@
 var Sprite = {
   create_player: function(container) {
     var element = $('<div class="sprite"/>').addClass(Tile.player.sprite_class);
-	element.css('position', 'absolute');
-	element.css('top', '0');
-	element.css('left', '0');
+        element.css('position', 'absolute');
+        element.css('top', '0');
+        element.css('left', '0');
     container.append(element);
     
     return {
-	  element: element,
-      move_to: function(sprite)
-	  {
-	  
-	  }
+      element: element,
+      move_to: function(sprite) {
+      
       }
-    }
-  ,
+    };
+  },
   create: function(container, tile) {
     var element = $('<div class="sprite"/>').addClass(tile.sprite_class);
     
-	
     container.append(element);
     
     return {
-	  element: element,
+      element: element,
       change_tile: function(new_tile) {
         element.removeClass(tile.sprite_class);
         
